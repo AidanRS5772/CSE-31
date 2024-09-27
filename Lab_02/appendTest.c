87efae9e5f437402f6882c9648903744015425df
AidanSgar <39351316+AidanSgar@users.noreply.github.com>
2024-09-27 16:46:21 -0700

    commit

--- Lab_02/appendTest.c
+++ Lab_02/appendTest.c
@@ -16,13 +16,14 @@
 char *append(char s1[], char s2[]) {
   int s1len = strlen(s1);
   int s2len = strlen(s2);
-  char *word = (char *)malloc((s1len + s2len) * sizeof(char));
+  char *word = (char *)malloc((s1len + s2len+1) * sizeof(char));
   for (int k = 0; k < s1len; k++) {
     *(word+k) = *(s1+k);
   }
   for (int k = 0; k < s2len; k++) {
     *(word + k + s1len) = *(s2+k);
   }
+  *(word + s1len + s2len) = '\0';
   return word;
 }
 
