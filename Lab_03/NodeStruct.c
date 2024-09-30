7face54dc4255828540ca0d73eeffb2af9f38c35
AidanSgar <39351316+AidanSgar@users.noreply.github.com>
2024-09-30 02:03:37 -0700

    code compiles and passes test cases when available

--- Lab_03/NodeStruct.c
+++ Lab_03/NodeStruct.c
@@ -7,15 +7,17 @@ struct Node {
     struct Node *next;
 };
 
-int main() {
+int main(void) {
 
     struct Node *head = (struct Node*) malloc(sizeof(struct Node));
     head->iValue = 5;
     head->fValue = 3.14;
 	
-	printf("Value of head: %ld", head);
-    printf("Adress of Head: %ld", &head);
-	
+	printf("Value of head: %p\n", head);
+    printf("Adress of head: %p\n", &head);
+    printf("Adress of iValue: %p\n", &head->iValue);
+    printf("Adress of fValue: %p\n", &head->fValue);
+    printf("Adress of next: %p\n", &head->next);
 	
 	return 0;
 }
